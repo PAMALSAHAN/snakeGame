047a95944948a9cc47b0140c376129c5d75b1ed4
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-10-12 18:39:32 +0530

    init function paint

---
+++ src/snakeGame/GamePlay.java
@@ -0,0 +1,23 @@
+package snakeGame;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JPanel;
+// Jpanel kiyanne light weight container ekak anith ewwa eke hang karala thiyaganna puluwan
+	  		
+
+public class GamePlay extends JPanel {  
+	
+	
+	public void paint(Graphics g) {
+		
+		//draw the image broder
+		g.setColor(Color.green);
+		g.drawRect(24, 10, 851, 55);
+		
+		//draw the title image
+		
+	}
+
+}
--- src/snakeGame/Main.java
+++ src/snakeGame/Main.java
@@ -7,11 +7,16 @@ import javax.swing.JFrame;
 public class Main {
 	public static void main(String[] args) {
 		JFrame obj=new JFrame();
-		obj.setBounds(500, 100, 600, 900); //mul deka x andy y e kiyanne frame eka thiyena thana 
-		obj.setBackground(Color.DARK_GRAY);
-		obj.setResizable(false);
+		obj.setBounds(500, 100, 500, 500); //mul deka x andy y e kiyanne frame eka thiyena thana 
+		obj.setBackground(Color.black);
+		obj.setResizable(true);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		GamePlay gameplay=new GamePlay();
+		
+		obj.add(gameplay);
+		
 	}
 
 }
