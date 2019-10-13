937d49f9e15bb224ee012032fe1548ab744164e1
PAMALSAHAN <43300747+PAMALSAHAN@users.noreply.github.com>
2019-10-13 20:14:08 +0530

    set background and put title

--- src/snakeGame/GamePlay.java
+++ src/snakeGame/GamePlay.java
@@ -3,20 +3,41 @@ package snakeGame;
 import java.awt.Color;
 import java.awt.Graphics;
 
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 // Jpanel kiyanne light weight container ekak anith ewwa eke hang karala thiyaganna puluwan
-	  		
+
 
 public class GamePlay extends JPanel {  
+	private ImageIcon titleImage;
 	
-	
-	public void paint(Graphics g) {
+	public void paint(Graphics g) {  //in here paint is the method automatically invoke wenawa 
 		
-		//draw the image broder
+		//draw the image border
 		g.setColor(Color.green);
-		g.drawRect(24, 10, 851, 55);
+		g.drawRect(24, 10, 851, 55); //snaketitle eke border eka x,y,with,and height rect ekak
 		
 		//draw the title image
+		titleImage=new ImageIcon("snaketitle.jpg");
+		titleImage.paintIcon(this, g, 25, 11);
+		
+		//draw border for gameplay
+		g.setColor(Color.blue);
+		g.drawRect(25,75,851,370);
+		
+		//background filling for game play
+		g.setColor(Color.black);
+		g.fillRect(26, 76, 850, 369);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
--- src/snakeGame/Main.java
+++ src/snakeGame/Main.java
@@ -7,15 +7,17 @@ import javax.swing.JFrame;
 public class Main {
 	public static void main(String[] args) {
 		JFrame obj=new JFrame();
-		obj.setBounds(500, 100, 500, 500); //mul deka x andy y e kiyanne frame eka thiyena thana 
-		obj.setBackground(Color.black);
-		obj.setResizable(true);
+		obj.setBounds(100, 100, 915, 500); //mul deka x andy y e kiyanne frame eka thiyena thana 
+		obj.setBackground(Color.DARK_GRAY);
+		obj.setResizable(false);
 		obj.setVisible(true);
 		obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		GamePlay gameplay=new GamePlay();
 		
 		obj.add(gameplay);
+		
+		
 		
 	}
 
